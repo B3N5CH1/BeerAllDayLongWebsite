441114ca41fbdcea67ba8f977f20b89d59130f64
Benjamin Graf <grafb6@bfh.ch>
2017-12-24 14:03:46 +0100

    Removed index.html, added ugly css

---
+++ home.php
@@ -0,0 +1,20 @@
+<?php
+
+    echo "<!DOCTYPE html>";
+        echo "<html lang='en'>";
+            echo "<head>";
+            echo "<meta charset='utf-8' />";
+            echo "<title>LeBeerShop</title>";
+            echo "<link rel='stylesheet' type='text/css' media='screen' href='css/Style.css' />";
+        echo "</head>";
+
+        echo "<body>";
+            echo "<h1>Beer</h1>";
+            echo "<p>I need beer!</p>";
+            echo "<p class = 'italic'>More beer!</p>";
+            echo "<img src='images/landscape-beer-glasses.jpg' alt='BEER!' />";
+            echo "<p class = 'bold'>Oispa kaljaa</p>";
+        echo "</body>";
+    echo "</html>";
+
+
