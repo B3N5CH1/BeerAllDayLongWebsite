9d36ffb4b8605f6e9537b6d3398de8a65fefb4d9
Quentin <flucq1@bfh.ch>
2018-01-10 10:33:29 +0100

    change link

--- home.php
+++ home.php
@@ -12,11 +12,9 @@
 			echo "<h1>Beer</h1>";
 			echo "<p>I need beer!</p>";
 			echo "<p>You can sign up here:</p>";
-			echo "<a href='./pages/signup.php'>Sign Up</a><br>";
+			echo "<a href='./pages/home.php?lang=en'>Home Page</a><br>";
 			echo "<p class = 'italic'>More beer!</p>";
 			echo "<img src='images/landscape-beer-glasses.jpg' alt='BEER!' />";
 			echo "<p class = 'bold'>Oispa kaljaa</p>";
 		echo "</body>";
 	echo "</html>";
-
-
