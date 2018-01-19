2cdc18042dbbab79ba7f0d7992e7bd53ff4a9ead
Benjamin Graf <grafb6@bfh.ch>
2018-01-19 17:18:42 +0100

    burger clean up

--- classes/burger.php
+++ classes/burger.php
@@ -280,150 +280,29 @@ function t($key) {
 			'de' => 'Passwort Verifizierung',
 			'fr' => 'Vérifier le mot de passe',
 			'en' => 'Verify Password'
+		),
+		 'inproperAccess' => array(
+	    'de' => 'Sie möchten auf ungeeignetem Weg auf diese Seite zugreiffen. Bitte Verwenden Sie das Login.',
+	    'fr' => 'Vous essayez d\'acceder à cette page d\'une manière peu orthodoxe. Utilisez s\'il vous plait le login.',
+	    'en' => 'You\'re accessing this page imroperly. Plese use the login.'
+	  ),
+	  'rmvBtn' => array(
+	    'de' => 'Aus dem Warenkorb entfernen',
+	    'fr' => 'Enlever du panier',
+	    'en' => 'Remove from Cart'
+	  ),
+		'congrats' => array (
+			'de' => 'Danke für ihren Einkau!',
+			'fr' => 'Merci pour votre commande!',
+			'en' => 'Thank you for your purchase!'
 		)
-// <<<<<<< HEAD
-// 	function t($key) {
-// 	  global $language;
-// 	  $texts = array(
-// 	      'url' => array(
-// 	        'de' => 'lang=de',
-// 	        'fr' => 'lang=fr',
-// 	        'en' => 'lang=en'
-// 	      ),
-// 	      'page' => array(
-// 	          'de'=>'Seite',
-// 	          'fr'=>'Page',
-// 	          'en'=>'Page'
-// 	      ),
-// 	      'content' => array(
-// 	          'de'=>'Unsere Produkte ',
-// 	          'fr'=>'Nos Produits ',
-// 	          'en'=>'Our Products '
-// 	      ),
-// 	      'button' => array(
-// 	          'de' => 'Zu den Produkten!',
-// 	          'fr' => 'Aux Produits!',
-// 	          'en' => 'To the Products!'
-// 	      ),
-// 	      'blondebeer' => array(
-// 	          'de' => 'Zu den Produkten!',
-// 	          'fr' => 'Aux Produits!',
-// 	          'en' => 'To the Products!'
-// 	      ),
-// 	      'darkbeer' => array(
-// 	          'de' => 'Zu den Produkten!',
-// 	          'fr' => 'Aux Produits!',
-// 	          'en' => 'To the Products!'
-// 	      ),
-// 	      'fruitbeer' => array(
-// 	          'de' => 'Zu den Produkten!',
-// 	          'fr' => 'Aux Produits!',
-// 	          'en' => 'To the Products!'
-// 	      ),
-// 	      'ipabeer' => array(
-// 	          'de' => 'Zu den Produkten!',
-// 	          'fr' => 'Aux Produits!',
-// 	          'en' => 'To the Products!'
-// 	      ),
-// 	      'specialbeer' => array(
-// 	          'de' => 'Zu den Produkten!',
-// 	          'fr' => 'Aux Produits!',
-// 	          'en' => 'To the Products!'
-// 	      ),
-// 	      'whitebeer' => array(
-// 	          'de' => 'Zu den Produkten!',
-// 	          'fr' => 'Aux Produits!',
-// 	          'en' => 'To the Products!'
-// 	      ),
-// 	      'cart' => array(
-// 	          'de' => 'Warenkorb',
-// 	          'fr' => "Panier d'achats",
-// 	          'en' => 'Cart'
-// 	      ),
-// 	      'about' => array(
-// 	          'de' => 'Über uns',
-// 	          'fr' => 'A propos de nous',
-// 	          'en' => 'About us'
-// 	      ),
-// 	      'policy' => array(
-// 	          'de' => 'Policy',
-// 	          'fr' => 'Policy',
-// 	          'en' => 'Policy'
-// 	      ),
-// 	      'contact' => array(
-// 	          'de' => 'Kontakt',
-// 	          'fr' => 'Contact',
-// 	          'en' => 'Contact'
-// 	      ),
-// 	      'currency' => array(
-// 	          'de' => 'SFR',
-// 	          'fr' => 'CHF',
-// 	          'en' => 'CHF'
-// 	      ),
-// 	      'addtocart' => array(
-// 	          'de' => 'Zum Warenkorb hinzufügen',
-// 	          'fr' => 'Ajout au panier',
-// 	          'en' => 'Add to cart'
-// 	      ),
-// 	      'about_text' => array(
-// 	        'de' => 'Wir sind eine Gruppe von Studenten an der Berner Fachhochschule und besuchen das Studium in Informatik.',
-// 	        'fr' => 'Nous sommes un groupe de deux étudiants de la Haute école spécialisé de Berne, étudiant les TI depuis trois ans.',
-// 	        'en' => 'We are a group of two student from the University of Applied Science of Bern, studying IT for the third year now.'
-// 	      ),
-// 	  'policy_text' => array(
-// 	    'de' => 'Hier ist unsere Policy',
-// 	    'fr' => 'Ceci est notre policy',
-// 	    'en' => 'This is our policy'
-// 	  ),
-// 	  'url' => array(
-// 	    'de' => 'lang=de',
-// 	    'fr' => 'lang=fr',
-// 	    'en' => 'lang=en'
-// 	  ),
-// 	  'fill' => array(
-// 	    'de' => 'Füllen Sie das Formular aus, wenn Sie bereits ein Kunde sind.<br>Wenn Sie noch keinen haben, registrieren Sie sich ',
-// 	    'fr' => 'Remplissez le formulaire pour vous inscrire.<br>Si vous n\'avez pas de compte vous pouvez vous enregistrer ',
-// 	    'en' => 'Fill in the form to log in as an existing customer.<br>If you don\'t have an account yet, you can sign up for one ',
-// 	  ),
-// 	  'here' => array(
-// 	    'de' => 'hier',
-// 	    'fr' => 'ici',
-// 	    'en' => 'here'
-// 	  ),
-// 	  'pw' => array(
-// 	    'de' => 'Passwort',
-// 	    'fr' => 'Mot de passe',
-// 	    'en' => 'Password'
-// 	  ),
-// 	  'loginError' => array(
-// 	    'de' => 'Fehler beim einloggen.<br>Passwort und Email stimmten mit keinem Eintrag in der Datenbank überein.<br>',
-// 	    'fr' => 'Problème lors du login.<br>Mot de passe et email ne correspondent à aucune entré de la base de donné.<br>',
-// 	    'en' => 'Could not log in.<br>Password and Email did not match any entry in the database.<br>'
-// 	  ),
-// 	  'inproperAccess' => array(
-// 	    'de' => 'Sie möchten auf ungeeignetem Weg auf diese Seite zugreiffen. Bitte Verwenden Sie das Login.',
-// 	    'fr' => 'Vous essayez d\'acceder à cette page d\'une manière peu orthodoxe. Utilisez s\'il vous plait le login.',
-// 	    'en' => 'You\'re accessing this page imroperly. Plese use the login.'
-//
-// 	  ),
-// 	  'rmvBtn' => array(
-// 	    'de' => 'Aus dem Warenkorb entfernen',
-// 	    'fr' => 'Enlever du panier',
-// 	    'en' => 'Remove from Cart'
-// 	  ),
-// 		'congrats' => array (
-// 			'de' => 'Danke für ihren Einkaufe !',
-// 			'fr' => 'Merci pour votre commande !',
-// 			'en' => 'Thank you for your purchase !!'
-// 		)
-//
-// 	  );
-// 	  if (isset($texts[$key][$language])) {
-// 	    return $texts[$key][$language];
-// 	  } else {
-// 	    return "[$key]";
-// 	  }
-// =======
+
+	  );
+	  if (isset($texts[$key][$language])) {
+	    return $texts[$key][$language];
+	  } else {
+	    return "[$key]";
+	}
 
 
 	);
